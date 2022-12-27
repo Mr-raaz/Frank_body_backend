df9e914ee96f3e65419a841ea5ca5921494643d3
Anshu Raj <fakereal477@gmail.com>
2022-12-27 22:38:08 +0530

    create user route added

--- routes/user.route.js
+++ routes/user.route.js
@@ -1,10 +1,16 @@
 const {Router} = require("express");
-
+const user = require('../models/user.model');
 
 const route = Router();
 
 
 
+route.post('/create' ,async (req,res)=>{
+   
+    let temp = await user.create(req.body);
+    
+    res.send("working");
+})
 
 
 
