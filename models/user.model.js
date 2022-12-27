37fb01445f6b148ba848bb80aee87cbfb0065aa5
Anshu Raj <fakereal477@gmail.com>
2022-12-27 19:15:58 +0530

    user schema created

--- models/user.model.js
+++ models/user.model.js
@@ -2,10 +2,35 @@ const mongoose = require("mongoose");
 
 const userModel = new mongoose.Schema({
 
+    name:{
+        type:String,
+        required:true,
+        trim:true
+    },
+    email:{
+        type:String,
+        required:true,
+        unique:true
+    },
+    password:{
+        type:String,
+        required:true
+    },
+    address:{
+        type:Array
+    },
+    orderhistory:{
+        type:Array
+    },
+    logintype:{
+        type:String,
+        required:true
+    },
+    cart:{
+        type:Array
+    }
+
 })
-
-
-
 
 const user = mongoose.model('users' , userModel);
 
