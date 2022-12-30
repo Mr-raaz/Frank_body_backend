ee1c77a75b87cec83242429da603484b542b8e19
Ayushsingh05 <singhayush28321@gmail.com>
2022-12-30 14:34:05 +0530

    done

--- routes/auth.route.js
+++ routes/auth.route.js
@@ -49,7 +49,7 @@ router.get("/logout", (req, res) => {
   res.redirect(CLIENT_URL);
 });
 
-router.get("/google", passport.authenticate("google", { scope: ["profile"] }));
+router.get("/google", passport.authenticate("google", { scope: ["email","profile"] }));
 
 router.get(
   "/google/callback",
@@ -59,7 +59,7 @@ router.get(
   })
 );
 
-router.get("/github", passport.authenticate("github", { scope: ["profile"] }));
+router.get("/github", passport.authenticate("github", { scope: ["email","profile"] }));
 
 router.get(
   "/github/callback",
