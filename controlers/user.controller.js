955b0790e286fcd3a7ba529b14d0411c5897bb06
hariprasanthmath <hariprasanthmath@gmail.com>
2022-12-30 13:14:13 +0530

    userside password and db pass comparison done with bcrypt module , token generated successfully

--- controlers/user.controller.js
+++ controlers/user.controller.js
@@ -1,5 +1,5 @@
 const user = require("../models/user.model")
-// const bscrypt = require('bcrypt');
+ const bscrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
 
 
@@ -13,13 +13,15 @@ async function userManualLogin({email, password}){
     }
     let samepass = false;
     console.log(typeof password, typeof alldata[0].password)
-    if(password + "" === ""+JSON.parse(JSON.stringify(alldata[0].password))){
-        console.log("condition passed");
-        samepass = true;
-    }
-    // const passwordMatch =  bscrypt.compare(password,alldata.password);
-    // console.log(passwordMatch, "hello");
-     if(samepass){
+    // let hashpass = bscrypt.hash(password);
+    // console.log(hashpass, "hashpassval");
+    // if(hashpass + "" === ""+JSON.parse(JSON.stringify(alldata[0].password))){
+    //     console.log("condition passed");
+    //     samepass = true;
+    // }
+      let passwordMatch =  bscrypt.compare(password,""+JSON.parse(JSON.stringify(alldata[0].password)));
+    //  console.log(passwordMatch, "hello");
+     if(passwordMatch){
         console.log("same pass is "+ samepass);
         const token = generateToken(alldata);
         return token;
