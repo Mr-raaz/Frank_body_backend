ec12ba202257304b4c5ead2c5709ac9901a0533a
Anshu Raj <fakereal477@gmail.com>
2022-12-31 16:57:34 +0530

    cart file added

--- client/src/Components/CartPage/Cart/Cart.js
+++ client/src/Components/CartPage/Cart/Cart.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import Navbar from '../../LandingPage/TopSection/Navbar/Navbar';
 import './Cart.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
@@ -9,12 +9,17 @@ import { useSelector } from 'react-redux';
 import Product from './Product';
 import { useState } from 'react';
 import { useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
 function Cart() {
 
+    const coupon_input = useRef();
     const cartData = useSelector((store) => store.cart);
 
     const [total_price , setTotalPrice] = useState(0);
     const [price_discount , setPriceDiscount] = useState(0);
+    const [coupon_status , setcouponStatus] = useState(false);
+    const [invalid_coupon , setValidCoupon] = useState(false);
+    const navigate = useNavigate();
     
     const[count_price , setPrice] = useState(0);
 
@@ -37,13 +42,27 @@ function Cart() {
         setPriceDiscount(Math.floor(temp/10 - Math.random() * 60))
     }
 
-
-    function handlecoupon(e){
-        party.confetti(e.target, {
-            count: party.variation.range(20, 40),
-        });
+    function handleProceed(){
+        navigate('/checkout')
     }
 
+
+    function handlecoupon(e){
+        let text = coupon_input.current.value;
+
+        if(text == "FIRST"){
+            setTotalPrice((prev) => Math.floor(prev/2))
+            setcouponStatus(true)
+            setValidCoupon(false);
+        } else {
+            setValidCoupon(true);
+        }
+    }
+
+    function handleremove(){
+        setTotalPrice((prev) => prev+prev)
+            setcouponStatus(false)
+    }
     useEffect(()=>{
         cartPrice();
     },[cartData])
@@ -111,15 +130,30 @@ function Cart() {
 
                             <div className='coupon_section'>
                                     <div>Discount Coupon</div>
-                                    <div className='coupon_input_box'>
-                                        <input type="text" />
+                                    {
+                                        coupon_status ? <>
+                                            <div className="coupon_box">
+                                                <div>
+                                                    Coupon Applied : <span className='remove' onClick={handleremove}>Remove</span>
+                                                </div>
+
+                                                <div>-&#x20B9; {total_price}</div>
+                                            </div>
+                                        </> : <><div className='coupon_input_box'>
+                                        <input type="text"  ref={coupon_input}/>
                                         <button onClick={handlecoupon}>Apply</button>
                                     </div>
+                                    {
+                                        invalid_coupon ? <p className='invalid_coupon'>Invalid Coupon Code</p> : null
+                                    }
+</> 
+                                    
+                                    }
                             </div>
 
 
                             <div className="proceed_btn">
-                                <button>PROCEED</button>
+                                <button onClick={handleProceed}>PROCEED</button>
                             </div>
                             
                         
