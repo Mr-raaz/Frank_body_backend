50945ad7b287ee203088cf41940ca2ebe241f3b1
raja00912cd <mdramiz025@gmail.com>
2022-12-31 19:27:58 +0530

    connected product page with backend

--- client/src/Components/Searched-Products/Searched-products.js
+++ client/src/Components/Searched-Products/Searched-products.js
@@ -1,10 +1,11 @@
 import React, { useEffect, useState } from 'react'
 import { useSearchParams } from 'react-router-dom'
+import style from "./SearchedProducts.css"
 
 function SearchedProducts() {
     let [searchParam, setParam] = useSearchParams();
     let search_query = searchParam.get('search_query')
-    let [searchedKey, setSearchedKey] = useState();
+    let [searchedData, setSearchedData] = useState([]);
 
     const getData = async () => {
         let res = await fetch(`https://frank-body-backend.vercel.app/products`)
@@ -17,7 +18,8 @@ function SearchedProducts() {
 
             return searched && (title.includes(searched) || cate.includes(searched));
         }).splice(0, 25);
-        console.log(products);
+        console.log(products)
+        setSearchedData(products);
     }
 
     useEffect(() => {
@@ -25,7 +27,25 @@ function SearchedProducts() {
     }, [searchParam])
     console.log(search_query);
     return (
-        <div>Searched-products</div>
+        <div>
+            {searchedData.map((elem, ind) => {
+                return (
+                    <div id='products_parent_div' key={ind + 1}>
+                        <div>
+                            <img src={elem.url_1} />
+                        </div>
+                        <div className='details_div'>
+                            <h3>{elem.prod_name}</h3>
+                            <h4>Best Price:  ₹ {elem.best_price}  <span></span></h4>
+                            <h5>₹ {elem.mrp} </h5>
+                            <p className='percent_span'> | ({parseInt(elem.best_price * 100 / elem.mrp)}%)</p>
+                            <h4>⭐⭐⭐⭐⭐</h4>
+                            <p>43 ratings & 15 reviews</p>
+                        </div>
+                    </div>
+                )
+            })}
+        </div>
     )
 }
 
